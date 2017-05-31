738116e45445e8904562de2ab3a89f9308ccf47b
weixiaohhh <3060106630@qq.com>
2017-05-31 13:30:00 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
 
-myapp = create_app('heroku')
+myapp = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(myapp)
 migrate = Migrate(myapp, db)
 
