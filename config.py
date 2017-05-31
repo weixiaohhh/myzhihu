127e06b5604f7509efcd2426c8eec8cca4682a5d
weixiaohhh <3060106630@qq.com>
2017-05-31 13:31:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
 	
     FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASK_MAIL_SENDER = 'Flasky Admin <flasky@example.com>
+    FLASK_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASK_ANSWERS_PER_PAGE = 5
     FLASK_COMMENTS_PER_PAGE = 5
